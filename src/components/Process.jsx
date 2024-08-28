db5fc5c8ce442f8201053e6dc91272cb6f6094e3
“WolfDen <wolfcodeden@gmail.com>
2024-08-28 15:19:04 +0200

    Card Exported to component

--- src/components/Process.jsx
+++ src/components/Process.jsx
@@ -1,21 +1,17 @@
 import React from "react";
-
+import Card from "./Card";
 const Process = ({ title, cardData }) => {
   return (
     <div className="flex flex-col justify-center items-center bg-white">
       <h1 className="my-10 text-4xl font-bold text-black">{title}</h1>
       <div className="flex justify-evenly items-center flex-col md:flex-row">
         {cardData.map((card, i) => (
-          <div
+          <Card
             key={i}
-            className="flex flex-col justify-center items-center gap-5 mx-8 "
-          >
-            <img className="w-full " src={card.imageSrc} alt={card.title} />
-            <h2 className="text-2xl font-semibold">{card.title}</h2>
-            <p className="text-center font-light text-lg mb-10">
-              {card.description}
-            </p>
-          </div>
+            title={card.title}
+            description={card.description}
+            imageSrc={card.imageSrc}
+          ></Card>
         ))}
       </div>
     </div>
